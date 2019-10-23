05b1bd58aab0f10372c6c93edd79b982a848d456
Manish Porwal <54469573+Manish1575@users.noreply.github.com>
2019-10-23 22:28:53 +0530

    adding star pattern 9

---
+++ StarPattern9.c
@@ -0,0 +1,39 @@
+// *********************************************************************
+//  Author - Manish Porwal
+//  Date :- 23rd October 2019
+//  Language :- C Language
+//  About :- Star Pattern 9
+//  *********************************************************************
+
+
+//  //////////////////////////////////////////////////////////////////////
+/*
+	   * * * * * * * * *
+             * * * * * * *
+   	       * * * * *
+   	         * * *
+      		   *
+
+
+*/
+//  //////////////////////////////////////////////////////////////////////
+
+
+#include<stdio.h>
+int main()
+{
+    int rows, i, j, space;
+    printf("Enter number of rows: ");
+    scanf("%d",&rows);
+    for(i=rows; i>=1; --i)
+    {
+        for(space=0; space < rows-i; ++space)
+            printf("  ");
+        for(j=i; j <= 2*i-1; ++j)
+            printf("* ");
+        for(j=0; j < i-1; ++j)
+            printf("* ");
+        printf("\n");
+    }
+    return 0;
+}
